fce2253a671e13123b4079381c9e4c3c201138bf
Roger <ro.rosamuel@hotmail.com>
2022-07-29 01:44:01 -0600

    cambios

--- React/src/components/Inicio.js
+++ React/src/components/Inicio.js
@@ -1,18 +1,67 @@
-//import axios from axios;
-import React, {Fragment, useState} from "react";
-import '../styles/registro.css'
-import Cards from './Cards';
-
+import axios from "axios";
+import React, { Fragment, useState } from "react";
+import "../styles/registro.css";
+import Cards from "./Cards";
 
 export default function Inicio(props) {
+  const [datos, setDatos] = useState({
+    filtro1: "",
+    filtro2: "",
+  });
 
-        return(
-            <div class="d-flex justify-content-center ">
-                <div className="centrado">
-                    <Cards></Cards>
-                </div>
-                
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setDatos({
+      ...datos,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const enviarDatos = (event) => {};
+
+  return (
+    <div class="d-flex justify-content-center ">
+      <div className="centrado">
+        <div className="row">
+          <div className="col">
+            <select
+              class="form-select"
+              aria-label="Default select example"
+              onChange={handleInputChange}
+              name="filtro1"
+            >
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div className="col">
+            <select
+              class="form-select"
+              aria-label="Default select example"
+              onChange={handleInputChange}
+              name="filtro2"
+            >
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
+        <div className="centrado">
+          <div className="row">
+            <div className="col">
+              <Cards></Cards>
             </div>
-        )
-}
+          </div>
+        </div>
 
+
+
+        
+      </div>
+    </div>
+  );
+}
--- React/src/components/registro.js
+++ React/src/components/registro.js
@@ -1,132 +1,204 @@
-import axios from 'axios';
-import React, {Fragment, useState} from "react";
-import '../styles/registro.css';
-import Login from './Login';
+import axios from "axios";
+import React, { Fragment, useState } from "react";
+import "../styles/registro.css";
+import Login from "./Login";
 
 export default function Registro(props) {
+  const [datos, setDatos] = useState({
+    n_id: "",
+    nombre: "",
+    apellido: "",
+    telefono: "",
+    direccion: "",
+    id_ciudad: "",
+    email: "",
+    clave: "",
+    f_nac: "",
+  });
 
-    const [datos, setDatos] = useState({
-        
-            n_id : "",
-            nombre: "",
-            apellido: "",
-            telefono: "",
-            direccion: "",
-            id_ciudad: "",
-            email: "",
-            clave: "",
-            f_nac: ""
-        
-      })
-    
-      const handleInputChange = (event) => {
-        console.log(event.target.value)
-        setDatos({
-          ...datos,
-          [event.target.name] : event.target.value
-        })
-      }
-    
-      const enviarDatos = (event) => {
-       const fecha = new Date(datos.f_nac)
-        const parseToString = (date) => {
-            return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
-        }
-        const newDatos = {
-            ...datos,
-            f_nac: parseToString(fecha)
-        }
-        const params = new URLSearchParams(newDatos);
-        
-        axios.post(
-            'http://localhost:8080/usuario/cliente/add',
-            params
-          )
-            .then((response) => console.log(response))
-            .catch((err) => console.log(err.message));
-      }
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setDatos({
+      ...datos,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-        return(
-        
-                <div class="d-flex justify-content-center">
-                    <div class="centrado">
-                <h1 class="display-4 text-center">Registro</h1>
-                <div class="centrado">
+  const enviarDatos = (event) => {
+    const fecha = new Date(datos.f_nac);
+    const parseToString = (date) => {
+      return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+    };
+    const newDatos = {
+      ...datos,
+      f_nac: parseToString(fecha),
+    };
+    const params = new URLSearchParams(newDatos);
 
-                <div class="row">
-                <div class="col">
-                    <label class="form-label">Nombre</label>
-                    <input type="text" class="form-control" placeholder="Aureliano" aria-label="First name" maxLength="25" onChange={handleInputChange} name="nombre"/>
-                </div>
-                <div class="col">
-                    <label class="form-label">Apellido</label>
-                    <input type="text" class="form-control" placeholder="Buendía" aria-label="Last name" maxLength="25" onChange={handleInputChange} name="apellido"/>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col">
-                    <label class="form-label">Número de Identidad</label>
-                    <input type="text" class="form-control" placeholder="0801-19XX-00XXX" aria-label="First name" maxLength="13" onChange={handleInputChange} name="n_id"/>
-                </div>
-                <div class="col">
-                    <label class="form-label">Teléfono</label>
-                    <input type="text" class="form-control" placeholder="99XX88XX" aria-label="First name" maxLength="8" onChange={handleInputChange} name="telefono"/>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col">
-                    <label class="form-label">Dirección</label>
-                    <input type="text" class="form-control" placeholder="Col. Macondo" aria-label="First name" maxLength="128" onChange={handleInputChange} name="direccion"/>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col">
-                    <label class="form-label">Ciudad</label>
-                    <input type="number" class="form-control" min="0" max="65"  placeholder="24" aria-label="First name" maxLength="128" onChange={handleInputChange} name="id_ciudad"/>
-                </div>
-                
-                </div>
-                <div class="row">
-                <div class="col">
-                    <label class="form-label">Email</label>
-                    <input type="text" class="form-control" placeholder="aureliano@buendia.com" aria-label="First name" name="email" onChange={handleInputChange}/>
-                </div>
-                <div class="col">
-                    <label class="form-label">Contraseña</label>
-                    <input type="password" class="form-control" placeholder="" aria-label="First name" name="clave" onChange={handleInputChange}/>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col">
-                    <label class="form-label">Fecha de Nacimiento</label>
-                    <input type="date" class="form-control" placeholder="aureliano@buendia.com" aria-label="First name" name="f_nac" onChange={handleInputChange}/>
-                </div>
-                </div>
-                <div class="switchpad">
-                
-                <div class="form-check form-switch">
-                <input defaultChecked="false" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="propietario"/>
-                <label class="form-check-label" for="flexSwitchCheckDefault">¿Rentara su maquinaria en la página?</label>
-                </div>
-                </div>
+    axios
+      .post("http://localhost:8080/usuario/cliente/add", params)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err.message));
+  };
 
-                <div class="centrado">
-                <div class="d-grid gap-2">
-                <button class="btn btn-warning" type="button" onClick={enviarDatos}>Registrarse</button>
-                </div>
-                <div><br></br></div>
+  return (
+    <div class="d-flex justify-content-center">
+      <div class="centrado">
+        <h1 class="display-4 text-center">Registro</h1>
+        <div class="centrado">
+          <div class="row">
+            <div class="col">
+              <label class="form-label">Nombre</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Aureliano"
+                aria-label="First name"
+                maxLength="25"
+                onChange={handleInputChange}
+                name="nombre"
+              />
+            </div>
+            <div class="col">
+              <label class="form-label">Apellido</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Buendía"
+                aria-label="Last name"
+                maxLength="25"
+                onChange={handleInputChange}
+                name="apellido"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <label class="form-label">Número de Identidad</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="0801-19XX-00XXX"
+                aria-label="First name"
+                maxLength="13"
+                onChange={handleInputChange}
+                name="n_id"
+              />
+            </div>
+            <div class="col">
+              <label class="form-label">Teléfono</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="99XX88XX"
+                aria-label="First name"
+                maxLength="8"
+                onChange={handleInputChange}
+                name="telefono"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <label class="form-label">Dirección</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Col. Macondo"
+                aria-label="First name"
+                maxLength="128"
+                onChange={handleInputChange}
+                name="direccion"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <label class="form-label">Ciudad</label>
+              <input
+                type="number"
+                class="form-control"
+                min="0"
+                max="65"
+                placeholder="24"
+                aria-label="First name"
+                maxLength="128"
+                onChange={handleInputChange}
+                name="id_ciudad"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <label class="form-label">Email</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="aureliano@buendia.com"
+                aria-label="First name"
+                name="email"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div class="col">
+              <label class="form-label">Contraseña</label>
+              <input
+                type="password"
+                class="form-control"
+                placeholder=""
+                aria-label="First name"
+                name="clave"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <label class="form-label">Fecha de Nacimiento</label>
+              <input
+                type="date"
+                class="form-control"
+                placeholder="aureliano@buendia.com"
+                aria-label="First name"
+                name="f_nac"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div class="switchpad">
+            <div class="form-check form-switch">
+              <input
+                defaultChecked="false"
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckDefault"
+                name="propietario"
+              />
+              <label class="form-check-label" for="flexSwitchCheckDefault">
+                ¿Rentara su maquinaria en la página?
+              </label>
+            </div>
+          </div>
 
-                <Login></Login>
+          <div class="centrado">
+            <div class="d-grid gap-2">
+              <button
+                class="btn btn-warning"
+                type="button"
+                onClick={enviarDatos}
+              >
+                Registrarse
+              </button>
+            </div>
+            <div>
+              <br></br>
+            </div>
 
-                </div>
-
-
-                </div> 
-                    </div>
-
-
-                </div>
-        
-        )
+            <Login></Login>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
