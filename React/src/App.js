f7ab2f7dd25a295d241be2e23a463a9d9c777e9b
Roger <ro.rosamuel@hotmail.com>
2022-07-28 23:13:35 -0600

    cambios frontend

--- React/src/App.js
+++ React/src/App.js
@@ -2,14 +2,23 @@ import React from 'react'
 import './App.css'
 import Barra from './components/barra';
 import Registro from './components/registro';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className='App'>
-      <Barra/>
-      <Registro/>
-    </div>
+    <Router>
+      <div className='App'>
+        <Barra/>
+        <div className='content'>
+          <Switch>
+            <Route path={'/'}>
+              <Registro/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   )
 }
 
