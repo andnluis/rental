636e1fa2d7201c6da51b4b360a637c5482ecbcd6
Roger Samuel Ponce Rodriguez <60912049+samuelrponce@users.noreply.github.com>
2022-08-10 02:34:43 -0600

    implementacion de nuevos componentes

--- React/src/App.js
+++ React/src/App.js
@@ -1,46 +1,56 @@
-import React from 'react'
-import './App.css'
-import Barra from './components/barra';
-import Registro from './components/registro';
-import Inicio from './components/Inicio';
-import SingIN from './components/singin';
-import InicioSesionGoogle from "./components/inicioSesionGoogle";
+import React from "react";
+import "./App.css";
+import { UsuarioProvider } from "./context/UsuarioContext";
+import Barra from "./components/barra";
+import Registro from "./components/registro";
+import Inicio from "./components/Inicio";
+import SingIN from "./components/singin";
+import InicioSesionGoogle from "./components/InicioSesionGoogle";
+import Maquinaria from "./components/maquinaria";
 import Verificacion from "./components/codigoVerificacion";
-import Footer from './components/footer';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
+import Perfil from "./components/perfil";
+import Footer from "./components/footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Barra/>
-        <div className='content'>
-          <Switch>
-            <Route exact path={'/'}>
-              <Inicio/>
-            </Route>
-            <Route path={'/Registro'}>
-              <Registro/>
-            </Route>
-            <Route path={'/InicioSesion'}>
-              <SingIN/>
-            </Route>
-            <Route path={'/Inicio'}>
-              <Inicio/>
-            </Route>
-            <Route path={'/verificacion'}>
-              <Verificacion/>
-            </Route>
-            <Route path={'/inicioGoogle'}>
-              <InicioSesionGoogle/>
-            </Route>
-          </Switch>
-          <Footer/>
+    <UsuarioProvider>
+      <Router>
+        <div className="App">
+          <Barra />
+          <div className="content">
+            <Switch>
+              <Route exact path={"/"}>
+                <Inicio />
+              </Route>
+              <Route path={"/Registro"}>
+                <Registro />
+              </Route>
+              <Route path={"/InicioSesion"}>
+                <SingIN />
+              </Route>
+              <Route path={"/Inicio"}>
+                <Inicio />
+              </Route>
+              <Route path={"/verificacion"}>
+                <Verificacion />
+              </Route>
+              <Route path={"/inicioGoogle"}>
+                <InicioSesionGoogle />
+              </Route>
+              <Route path={"/maquinaria"}>
+                <Maquinaria />
+              </Route>
+              <Route path={"/perfil"}>
+                <Perfil />
+              </Route>
+            </Switch>
+            <Footer />
+          </div>
         </div>
-      </div>
-    </Router>
-  )
+      </Router>
+    </UsuarioProvider>
+  );
 }
 
-export default App
+export default App;
