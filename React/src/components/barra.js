c77ac08f141f569685ca014f04b895d3719ebea5
Roger Samuel Ponce Rodriguez <60912049+samuelrponce@users.noreply.github.com>
2022-08-10 02:27:11 -0600

    barra con login

--- React/src/components/barra.js
+++ React/src/components/barra.js
@@ -1,31 +1,50 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { Fragment, useState, useContext } from "react";
+import { UsuarioContext } from "../context/UsuarioContext";
 
 const Barra = () => {
 
+  const { userData} = useContext(UsuarioContext);
+
+
+  const navbar = () =>{
+    if(userData){
+      
+      if(userData.rol[0]==="ROL_PROPIETARIO"){
+        return propietario;
+      }else if(userData.rol[0]==="ROL_CLIENTE"){
+        return cliente;
+      }
+    }else{
+      return invitado;
+    }
+  }
+
   const cliente = (
-  <>
+
   <div class="d-grid gap-2 d-md-flex justify-content-md-end">
         <Link to="/" type="button" class='btn btn-warning me-md-2'>Ver Catalogo</Link>
         <Link to="/Carro" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "cart.svg"} width="20px" height={"20px"}></img></Link>
-        <Link to="/InicioSesion" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "profile.svg"} width="20px" height={"20px"}></img></Link>
+        <Link to="/InicioSesion" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "profile.svg"} width="20px" height={"20px"}></img>{userData.username}</Link>
+        
         <Link to="/InicioSesion" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "out.svg"} width="20px" height={"20px"}></img></Link>
     </div>
-  </>
+ 
   );
   
   const propietario = (
-    <>
+
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
           <Link to="/" type="button" class='btn btn-warning me-md-2'>Ver Catalogo</Link>
-          <Link to="/Carro" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "plus.svg"} width="20px" height={"20px"}></img></Link>
+          <Link to="/maquinaria" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "plus.svg"} width="20px" height={"20px"}></img></Link>
           <Link to="/InicioSesion" type="button" class='btn btn-warning me-md-2'>Dashboard</Link>
           <Link to="/Carro" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "cart.svg"} width="20px" height={"20px"}></img></Link>
-    <Link to="/InicioSesion" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "profile.svg"} width="20px" height={"20px"}></img></Link>
+          <Link to="/perfil" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "profile.svg"} width="20px" height={"20px"}></img>{userData.username}</Link>
           <Link to="/InicioSesion" type="button" class='btn btn-warning me-md-2'><img src={process.env.PUBLIC_URL + "out.svg"} width="20px" height={"20px"}></img></Link>
       </div>
-    </>
+
     );
+
     const invitado = (
       <div class="d-grid gap-2 d-md-flex justify-content-md-end">
         <Link to="/" type="button" class='btn btn-warning me-md-2'>Ver Catalogo</Link>
@@ -44,7 +63,7 @@ const Barra = () => {
           width="320px"
         ></img>
       </a>
-        {cliente}
+        {navbar()}
     </nav>
   );
   //coment
