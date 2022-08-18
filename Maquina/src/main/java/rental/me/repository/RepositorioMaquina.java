da433205c2784504ce93b1b3184621be97cae789
Luis Andino <landinom@unah.hn>
2022-08-18 07:12:15 -0600

    cambio de disponibilidad en maquinas

--- Maquina/src/main/java/rental/me/repository/RepositorioMaquina.java
+++ Maquina/src/main/java/rental/me/repository/RepositorioMaquina.java
@@ -13,18 +13,18 @@ public interface RepositorioMaquina extends CrudRepository<Maquina, Integer> {
 
 	Maquina findById (int id_maq);
 
-	@Query(value = "select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo  where tipo_maquina.id_tipo=?1",nativeQuery = true)
+	@Query(value = "select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo  where tipo_maquina.id_tipo=?1 and maquina.disponible = true",nativeQuery = true)
 	List<Maquina> filtrarTipo(int tipo);
 
 
 	@Query(value = "select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo inner join " +
-			"subcategoria on tipo_maquina.id_subcat = subcategoria.id_subcat where subcategoria.id_subcat=?1",nativeQuery = true)
+			"subcategoria on tipo_maquina.id_subcat = subcategoria.id_subcat where subcategoria.id_subcat=?1 and maquina.disponible = true",nativeQuery = true)
 	List<Maquina> filtrarSubcategoria(int subcat);
 
 	@Query(value = "select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo inner join subcategoria on\n" +
 			"tipo_maquina.id_subcat = subcategoria.id_subcat inner join categoria on \n" +
 			"categoria.id_cat = subcategoria.id_cat\n" +
-			"where categoria.id_cat=?1",nativeQuery = true)
+			"where categoria.id_cat=?1 and maquina.disponible",nativeQuery = true)
 	List<Maquina> filtrarCategoria(int subcat);
 
 	@Query(value = "select count(*) from maquina where id_prop = ?1", nativeQuery = true)
@@ -33,12 +33,12 @@ public interface RepositorioMaquina extends CrudRepository<Maquina, Integer> {
 	@Query(value = "select * from maquina where id_prop = ?1", nativeQuery = true)
 	List<Maquina> maquinasDeUsuario(int id);
 
-	@Query(value = "select * from maquina where id_man = ?1", nativeQuery = true)
+	@Query(value = "select * from maquina where id_man = ?1 and disponible = true", nativeQuery = true)
 	List<Maquina> maquinasManufacturador(int id);
 
-	@Query(value = "select * from maquina where ubicacion = ?1", nativeQuery = true)
+	@Query(value = "select * from maquina where ubicacion = ?1 and disponible = true", nativeQuery = true)
 	List<Maquina> maquinaCiudad(int id);
-	@Query(value = "select * from maquina inner join ciudad on ciudad.id_ciudad = maquina.ubicacion where ciudad.id_dep = ?1", nativeQuery = true)
+	@Query(value = "select * from maquina inner join ciudad on ciudad.id_ciudad = maquina.ubicacion where ciudad.id_dep = ?1 and maquina.disponible = true", nativeQuery = true)
 	List<Maquina> maquinaDept(int id);
 
 	@Query(value="select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo inner join subcategoria on \n" +
@@ -46,28 +46,28 @@ public interface RepositorioMaquina extends CrudRepository<Maquina, Integer> {
 			"categoria.id_cat = subcategoria.id_cat \n" +
 			"where categoria.id_cat =:categoria_id " +
 			"and maquina.ubicacion =:ciudad_id " +
-			"and maquina.id_man =:manofacturador_id" ,nativeQuery = true)
+			"and maquina.id_man =:manofacturador_id and maquina.disponible = true" ,nativeQuery = true)
 	List<Maquina> maquinaDinamica( int ciudad_id,int categoria_id,int manofacturador_id);
 
 	@Query(value="select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo inner join subcategoria on \n" +
 			"tipo_maquina.id_subcat = subcategoria.id_subcat inner join categoria on \n" +
 			"categoria.id_cat = subcategoria.id_cat \n" +
 			"where categoria.id_cat =:categoria_id \n" +
-			"and maquina.ubicacion =:ciudad_id ",nativeQuery = true)
+			"and maquina.ubicacion =:ciudad_id and maquina.disponible = true",nativeQuery = true)
 	List<Maquina> maquinaCuidadCategoria(int ciudad_id,int categoria_id);
 
 	@Query(value="select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo inner join subcategoria on \n" +
 			"tipo_maquina.id_subcat = subcategoria.id_subcat inner join categoria on \n" +
 			"categoria.id_cat = subcategoria.id_cat \n" +
 			"where maquina.id_man =:manofacturador_id \n" +
-			"and maquina.ubicacion =:ciudad_id ",nativeQuery = true)
+			"and maquina.ubicacion =:ciudad_id and maquina.disponible = true",nativeQuery = true)
 	List<Maquina> maquinaCuidadManofacturador(int ciudad_id,int manofacturador_id);
 
 	@Query(value="select * from maquina inner join tipo_maquina on tipo_maquina.id_tipo = maquina.id_tipo inner join subcategoria on \n" +
 			"tipo_maquina.id_subcat = subcategoria.id_subcat inner join categoria on \n" +
 			"categoria.id_cat = subcategoria.id_cat \n" +
 			"where maquina.id_man =:manofacturador_id \n" +
-			"and categoria.id_cat =:categoria_id ",nativeQuery = true)
+			"and categoria.id_cat =:categoria_id and maquina.disponible = true",nativeQuery = true)
 	List<Maquina> maquinaCategoriaManofacturador(int categoria_id,int manofacturador_id);
 
 }
